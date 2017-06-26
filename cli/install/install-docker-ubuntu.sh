577e6361915f438298159b3367ac207b33e8eb26
knutole <knutole@mapic.io>
2017-06-27 00:38:43 +0200

    install docker with apt

--- cli/install/install-docker-ubuntu.sh
+++ cli/install/install-docker-ubuntu.sh
@@ -57,10 +57,16 @@ if command_exists docker; then
     echo "$DOCKER_VERSION already installed"
 else 
     # install docker
-    CODENAME=$(lsb_release -cs)
-    PKG="docker-ce_17.03.0~ce-0~ubuntu-"$CODENAME"_amd64.deb"
-    curl -L https://download.docker.com/linux/ubuntu/dists/$CODENAME/pool/stable/amd64/$PKG > /tmp/$PKG | abort "Couldn't download $PKG"
-    sudo dpkg --force-confdef --force-confold -i /tmp/$PKG
+    # CODENAME=$(lsb_release -cs)
+    # PKG="docker-ce_17.03.0~ce-0~ubuntu-"$CODENAME"_amd64.deb"
+    # curl -L https://download.docker.com/linux/ubuntu/dists/$CODENAME/pool/stable/amd64/$PKG > /tmp/$PKG || abort "Couldn't download $PKG"
+    # sudo dpkg --force-confdef --force-confold -i /tmp/$PKG
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update -y
+    apt-cache policy docker-ce
+    sudo apt-get -f install
+    sudo apt-get install -y docker-ce
     DOCKER_VERSION=$(docker -v)
     echo "$DOCKER_VERSION installed"
 fi
