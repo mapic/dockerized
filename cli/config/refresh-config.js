5a73f56c2723be83a4b8420fe8c86a421beaba99
knutole <knutole@mapic.io>
2017-07-25 16:00:42 +0000

    config: refresh engine config with domains

--- cli/config/refresh-config.js
+++ cli/config/refresh-config.js
@@ -9,6 +9,8 @@ var ENGINE_CONFIG_PATH  = CONFIG_FOLDER + "engine.config.js";
 var REDIS_CONFIG_PATH   = CONFIG_FOLDER + "redis.conf";
 var NGINX_CONFIG_PATH   = CONFIG_FOLDER + "nginx.conf";
 
+
+
 // check if folder exists
 if (!fs.existsSync(CONFIG_FOLDER)) {
     console.log(CONFIG_FOLDER, 'does not exist. Quitting!');
@@ -52,6 +54,41 @@ engineConfig.serverConfig.mongo.url =  'mongodb://' + mongo_config.user + ':' + 
 engineConfig.serverConfig.redis.layers.auth = redisPassString;
 engineConfig.serverConfig.redis.stats.auth = redisPassString;
 engineConfig.serverConfig.redis.temp.auth = redisPassString;
+
+// get env
+var MAPIC_DOMAIN = process.env.MAPIC_DOMAIN;
+var domain_split = MAPIC_DOMAIN.split('.').reverse();
+var MAPIC_ROOT_DOMAIN = domain_split[1] + '.' + domain_split[0];
+var MAPIC_SUBDOMAIN = domain_split.reverse()[0];
+var MAPIC_BASE_URL = 'https://' + MAPIC_DOMAIN;
+engineConfig.serverConfig.portalServer.uri = 'https://' + MAPIC_DOMAIN + '/';
+engineConfig.clientConfig.servers.portal = 'https://' + MAPIC_DOMAIN + '/';
+engineConfig.clientConfig.servers.subdomain = 'https://{s}.' + MAPIC_ROOT_DOMAIN + '/';
+engineConfig.clientConfig.servers.tiles.uri = 'https://{s}.' + MAPIC_ROOT_DOMAIN + '/v2/tiles/';
+engineConfig.clientConfig.servers.tiles.subdomains = [
+    'tiles-a-' + MAPIC_SUBDOMAIN, 
+    'tiles-b-' + MAPIC_SUBDOMAIN, 
+    'tiles-c-' + MAPIC_SUBDOMAIN, 
+    'tiles-d-' + MAPIC_SUBDOMAIN 
+];
+engineConfig.clientConfig.servers.cubes.uri = 'https://{s}.' + MAPIC_ROOT_DOMAIN + '/v2/cubes/';
+engineConfig.clientConfig.servers.cubes.subdomains = engineConfig.clientConfig.servers.tiles.subdomains;
+engineConfig.clientConfig.servers.proxy.uri = 'https://{s}.' + MAPIC_ROOT_DOMAIN + '/v2/tiles/';
+engineConfig.clientConfig.servers.proxy.subdomains = [
+    'proxy-a-' + MAPIC_SUBDOMAIN, 
+    'proxy-b-' + MAPIC_SUBDOMAIN, 
+    'proxy-c-' + MAPIC_SUBDOMAIN, 
+    'proxy-d-' + MAPIC_SUBDOMAIN 
+];
+engineConfig.clientConfig.servers.utfgrid.uri = 'https://{s}.' + MAPIC_ROOT_DOMAIN + '/v2/tiles/';
+engineConfig.clientConfig.servers.utfgrid.subdomains = [
+    'grid-a-' + MAPIC_SUBDOMAIN, 
+    'grid-b-' + MAPIC_SUBDOMAIN, 
+    'grid-c-' + MAPIC_SUBDOMAIN, 
+    'grid-d-' + MAPIC_SUBDOMAIN 
+];
+
+
 var engineJsonStr = 'module.exports = ' + JSON.stringify(engineConfig, null, 2);
 var content = fs.readFileSync(ENGINE_CONFIG_PATH);
 content = content.toString('utf8');
@@ -60,8 +97,8 @@ fs.writeFileSync(ENGINE_CONFIG_PATH , engineJsonStr, 'utf-8');
 // redis
 updateRedisConfig(REDIS_CONFIG_PATH);
 
+
 // nginx
-var MAPIC_DOMAIN = process.env.MAPIC_DOMAIN;
 var nginxConfig = fs.readFileSync(NGINX_CONFIG_PATH);
 nginxConfig = nginxConfig.toString('utf8');
 var replace_text = 'server_name                 ' + MAPIC_DOMAIN + ';'
